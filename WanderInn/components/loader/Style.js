10f9054774fbc938ada842dd90ec74dcf5f0b3a4
Barun Karmakar <kumarbarun137@gmail.com>
2024-05-10 17:18:34 +0530

    Registration 10.05.2024

---
+++ WanderInn/components/loader/Style.js
@@ -0,0 +1,11 @@
+import { colors } from "@/constants/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.screenBg,
+    },
+});
