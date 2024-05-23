5cecda538ba30eb2c5aca9e9e9d1a4e3e4780b86
Saikat Ruj <164467233+SaikatRuj@users.noreply.github.com>
2024-05-23 18:22:10 +0530

    schemas and models

--- wanderin-backend/models/hotelModel.js
+++ wanderin-backend/models/hotelModel.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 
 const hotelSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    location: { type: String, required: true },
+    location: {
+        type: { type: String, default: "Point" },
+        coordinates: {
+            type: [Number],
+            required: true,
+            validate: {
+                validator: function (value) {
+                    return value.length === 2 && value.every(v => typeof v === 'number');
+                },
+                message: 'Coordinates must be an array of two numbers (longitude and latitude).'
+            }
+        }
+    },
     amenities: [String],
     starRating: { type: Number, min: 1, max: 5 },
     rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }],
--- wanderin-backend/routes/route.js
+++ wanderin-backend/routes/route.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
+const reviewController = require('../controllers/reviewController');
+const dealController = require('../controllers/dealController');
+const contactController = require('../controllers/contactController');
 const hotelController = require('../controllers/hotelController');
+const bookingController = require('../controllers/bookingController');
+const faqController = require('../controllers/faqController');
 // const uploadFile = require('../controllers/fileUploadController').uploadFile;
 // const notificationController = require('../controllers/notificationController');
 
@@ -21,8 +26,34 @@ router.post('/updateUser', authMiddleware.isAuth, userController.updateUser);
 router.post('/addHotel', authMiddleware.isAuth, hotelController.addHotel);
 router.get('/getHotels', authMiddleware.isAuth, hotelController.getHotels);
 router.get('/getHotelById/:id', authMiddleware.isAuth, hotelController.getHotelById);
-router.post('/updateHotel/:id', authMiddleware.isAuth, hotelController.updateHotel);
+router.put('/updateHotel/:id', authMiddleware.isAuth, hotelController.updateHotel);
 router.delete('/deleteHotel/:id', authMiddleware.isAuth, hotelController.deleteHotel);
 router.delete('/deleteRoomById/:id', authMiddleware.isAuth, hotelController.deleteRoomById);
+router.post('/hotelsSearch', authMiddleware.isAuth, hotelController.searchHotels);
+router.post('/hotelSearctFilter', authMiddleware.isAuth, hotelController.searchHotelsFilter);
+
+//Booking APIs
+router.post('/bookHotel', authMiddleware.isAuth, bookingController.bookHotel);
+router.post('/updateBooking/:id', authMiddleware.isAuth, bookingController.updateBooking);
+router.post('/cancelBooking/:id', authMiddleware.isAuth, bookingController.cancelBooking);
+
+//Review APIs
+router.post('/submitReview', authMiddleware.isAuth, reviewController.submitReview);
+router.get('/getReviewsByHotelId/:id', authMiddleware.isAuth, reviewController.getReviewsByHotelId);
+
+//FAQ API's
+router.post('/addFaqs', authMiddleware.isAdmin, faqController.createFAQ);
+router.get('/faqs', faqController.getFAQs);
+router.put('/updateFaqById/:id', faqController.updateFAQ);
+router.delete('/deleteFaqById/:id', faqController.deleteFAQ);
+
+//contact us
+router.post('/contact', contactController.contactCustomerService);
+
+//Deals controller
+router.post('/createDeal', dealController.createDeal);
+router.get('/getAllDeals', dealController.getDeals);
+router.put('/editDealById/:id', dealController.updateDeal);
+router.delete('/deleteDealById/:id', dealController.deleteDeal);
 
 module.exports = router;
