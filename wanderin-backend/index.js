650fd8bf7d405c8dc2bda68578075f69f69ca810
Barun Karmakar <kumarbarun137@gmail.com>
2025-01-24 12:10:44 +0530

    Update index.js

--- wanderin-backend/index.js
+++ wanderin-backend/index.js
@@ -12,7 +12,7 @@ const route = require('./routes/route');
 const admin = require('firebase-admin');
 
 // Replace with the path to your service account key file
-const serviceAccount = require('./serviceAccount/wanderInn-config.json');
+const serviceAccount = require('./serviceAccount/firebase-admin-sdk.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
